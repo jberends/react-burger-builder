9b137f7ad98f435f05a6419b991392e5aff7cace
Jochem Berends <jochem.berends@ke-works.com>
2018-08-31 14:35:02 +0200

    initial commit

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const burger = (props) => {
+  return (
+    <Aux></Aux>
+  );
+};
+
+export default burger;
---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Layout from '../components/Layout/Layout'
+
+import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p><strong>Test</strong></p>
+        </Layout>
+        <BurgerBuilder>
+          <p>Build controls</p>
+        </BurgerBuilder>
+      </div>
+    );
+  }
+}
+
+export default App;
