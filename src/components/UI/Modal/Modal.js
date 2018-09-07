3e657d7971cdf136c2b79a0d2c0d1df8edb230ff
Jochem Berends <jochem.berends@ke-works.com>
2018-09-07 16:12:43 +0200

    sec8 les142 shoudl comp update

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import classes from './Modal.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
+
+class Modal extends Component {
+
+    componentWillUpdate() {
+        console.log('[Modal] inside componentWillUpdate');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Modal] inside should COmponentn Update');
+        return nextProps.show !== this.props.show
+    }
+
+
+    render() {
+
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+};
+
+export default Modal;
